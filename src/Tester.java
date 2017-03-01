bb084ba1d7425bf0786472850bd70960455e3d22
rofelganado <rganado@ryerson.ca>
2017-03-01 15:35:50 -0500

    something

---
+++ src/Tester.java
@@ -0,0 +1,36 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class Tester extends TestCase{
+	/**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public Tester( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( Tester.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    public void testTest() {
+    	Test test = new Test();
+    	System.out.println(test.print());
+    }
+}
